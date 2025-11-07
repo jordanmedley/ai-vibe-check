75556fc302041458798ede41cc4981d765822e26
jordanmedley <jordanmedley1@gmail.com>
2025-11-06 22:06:15 -0500

    Update emotion_app.py
    removed use_cache=True

--- emotion_app.py
+++ emotion_app.py
@@ -151,7 +151,6 @@ def run_workflow_on_image(img_bytes: io.BytesIO) -> dict | list:
         workspace_name=WORKSPACE_NAME,
         workflow_id=WORKFLOW_ID,
         images={"image": b64},  # matches your working workflow output
-        use_cache=True,
     )
 
 # =========================
@@ -276,3 +275,4 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
+
